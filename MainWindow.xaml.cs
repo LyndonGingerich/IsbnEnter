2e1140fad0fc7ca8bc4687b679fe4bffcf44ebce
Lyndon Gingerich <GingerichLyndon@Gmail.com>
2022-11-07 19:36:37 -0600

    Merge logical pattern

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -83,7 +83,7 @@ internal record struct CheckedIsbn {
   public static CheckedIsbn? Create(string isbn) {
     bool IsFormattedAsIsbn(string s) {
       char[] validChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'X' };
-      return s.AsEnumerable().All(c => validChars.Contains(c)) && (s.Length == 10 || s.Length == 13);
+      return s.AsEnumerable().All(c => validChars.Contains(c)) && s.Length is 10 or 13;
     }
 
     return IsFormattedAsIsbn(isbn) ? new CheckedIsbn(isbn) : null;
